33c45ce98fb9fd199d79d17afaa1966162c1e394
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-12 16:25:10 +0100

    Capacitadors basic

---
+++ alumini/alumini.ino
@@ -0,0 +1,36 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2);
+
+int led1 = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  Serial.begin(9600);
+
+  pinMode(led1, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  long start = millis();
+  long total1 = cs_4_2.capacitiveSensor(30);
+
+  Serial.print(millis() - start);
+  Serial.print("\t");
+
+  Serial.println(total1);
+
+  delay(10);
+
+  if(total1 > 600){
+    digitalWrite(led1, HIGH);
+  } else {
+    digitalWrite(led1, LOW);
+  }
+ 
+}
