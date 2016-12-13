6b780871cf80709983bbf12acad33d6476d20fe3
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-13 13:34:29 +0100

    Read & Move & Recordes best values
    1. Servo moves each 0º - 5º - ...
    2. Sensor reads
    3. Compares old proximity value with actual value
    (changes the best distance and the correspondant position recorded if it's lower)
    4. Finishes the 10º-170º turn and servo goes to the best position
    5. Refresh the values and do the same

--- JOKULL_PROVA_SERVOPROX/JOKULL_PROVA_SERVOPROX.ino
+++ JOKULL_PROVA_SERVOPROX/JOKULL_PROVA_SERVOPROX.ino
@@ -1,80 +1,56 @@
-// AServo activated by an HC-SR04 via Arduino.
-/*
-  This sketch reads a HC-SR04 ultrasonic rangefinder and returns the
-  distance to the closest object in range. To do this, it sends a pulse
-  to the sensor to initiate a reading, then listens for a pulse
-  to return. The length of the returning pulse is proportional to
-  the distance of the object from the sensor.
-
-  The Arduino then takes this information and initiates a series of
-  sweeps. A sweep is defined as the servo moving its horn from the
-  0 position to 180 and back to 0 then to -180 and back to 0.
-
-  This code was developed partially from Ping))) code found in the
-  public domain written by David A. Mellis, and adapted to the HC-SRO4
-  by Tautvidas Sipavicius, servo code was provided the sweep example
-  that is include in the Arduino IDE, while other portions were written
-  by Charles Gantt and Curtis Gauger from
-  http://www.themakersworkbench.com.
-
-  This code was written for a tutorial on http://www.themakersworkbench.com,
-  and was designed for use in a project built by
-  The Young Makers of Greater Augusta.
-*/
-
-//Tell the Arduino Ide to include the Servo.h library.
-
 #include <Servo.h>
 
 const int trigPin = 6;
 const int echoPin = 5;
 Servo myservo;
-int pos = 0;
-int lastpos = 0;
-int greenLed = 3, redLed = 2;
+int pos;
+int para;
+int cm;
+int bestValue;
+int best;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);// attaches the servo on pin 9 to the servo object
+  myservo.attach(9);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
 }
 
 void loop() {
-  // and the distance result in centimeters:
-  long duration, cm;
-  //ultrasensor working
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-//move the servo   
-  if (distance <= 15)  {
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, HIGH);
-    myservo.write(170);
-    delay(450);
+  Serial.print("Start");
+  for (pos = 10; pos <= 170; pos += 5) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);       // tell servo to go to position in variable 'pos'
+    cm = bestValue;
+    cm = ping(TriggerPin, EchoPin);
+    Serial.print("Distancia: ");
+    Serial.println(cm);
+    delay(5);
 
-    digitalWrite(redLed, LOW);
-    myservo.write(90);
-    delay(450);
-    
-    digitalWrite(redLed, HIGH);
-    myservo.write(0);
-    delay(450);
-
-    digitalWrite(redLed, LOW);
-    myservo.write(90);
-  } else {
-    digitalWrite(redLed, LOW);
-    digitalWrite(greenLed, HIGH); 
+    if (bestValue > cm) {
+      bestValue = cm;
+      best = pos;
+    }
   }
 
-long microsecondsToCentimeters(long microseconds) {
-return microseconds / 29 / 2;
+  // back to start
+  myservo.write(best);              // tell servo to go to position in variable 'pos'
+  delay(1000);
+  bestValue = 1000000;
+  Serial.println(best);
 }
+
+int ping(int TriggerPin, int EchoPin) {
+  long duration, distanceCm;
+
+  digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+
+  duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+
+  distanceCm = duration * 10 / 292 / 2;  //convertimos a distancia, en cm
+  return distanceCm;
 }
