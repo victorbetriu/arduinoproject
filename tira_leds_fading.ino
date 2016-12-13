62d70cec6f12cd480b9ddc8f6a75428bf1335340
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-13 09:09:21 +0100

    Autoformat + fading slower

--- tira_leds_fading.ino
+++ tira_leds_fading.ino
@@ -1,11 +1,8 @@
+int ledPin = 3;
 
 void setup()
 { Serial.begin(9600);
-
-
-
-  pinMode( 6, OUTPUT);      // Pin PWM para regular la base del transistor
-
+  pinMode( ledPin, OUTPUT); 
   delay(1000);   // Pequeño retraso para asegurarnos de que la tension es estable
   Serial.println("Iniciando la fuente");
 }
@@ -13,13 +10,12 @@ void setup()
 void loop() {
   if (millis() % 1000 == 0) {
     for (int i = 0; i < 256; i++) {
-      analogWrite (6, i);       // Ponemos la tension de control
-      int iinversa=map(i,0,255,255,0);
-      analogWrite(6, iinversa);
-      delay(1);
+      analogWrite (6, i);
     }
-  }
-
-  
+  } 
+  if (millis()%1000==500){
+     for(int i=255; i>0;i--){
+      analogWrite(6, i);
+    }
+  }  
 }
-
