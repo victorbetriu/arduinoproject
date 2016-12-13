8b91d19a09e0bb2b2a42efd6c7ff5842a20fa253
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-13 13:23:34 +0100

    servo moves 0º - 90º -...

--- JOKULL_PROVA_SERVOPROX/JOKULL_PROVA_SERVOPROX.ino
+++ JOKULL_PROVA_SERVOPROX/JOKULL_PROVA_SERVOPROX.ino
@@ -25,15 +25,13 @@
 //Tell the Arduino Ide to include the Servo.h library.
 
 #include <Servo.h>
-//Setup the variables for the HC-SR04
+
 const int trigPin = 6;
 const int echoPin = 5;
-// create servo object to control a servo
-// a maximum of eight servo objects can be created
 Servo myservo;
-// variable to store the servo position
 int pos = 0;
 int lastpos = 0;
+int greenLed = 3, redLed = 2;
 
 void setup() {
   Serial.begin(9600);
@@ -41,85 +39,42 @@ void setup() {
 }
 
 void loop() {
-  // establish variables for duration of the ping,
-  // and the distance result in inches and centimeters:
+  // and the distance result in centimeters:
   long duration, cm;
-  // The sensor is triggered by a HIGH pulse of 10 or
-  // more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  //ultrasensor working
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
   // convert the time into a distance
   cm = microsecondsToCentimeters(duration);
-  //Tell the Arduino to print the measurement in the serial console
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  // This if-else statement tells the Arduino at what distance
-  // it should trigger the servo, what the servo should do,
-  // and what it should do if the distance is too far away.
-  if (cm <= 20) {
-    myservo.write(pos);
-  } else if (cm >= 20) {
-    sweep();
+//move the servo   
+  if (distance <= 15)  {
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, HIGH);
+    myservo.write(170);
+    delay(450);
+
+    digitalWrite(redLed, LOW);
+    myservo.write(90);
+    delay(450);
+    
+    digitalWrite(redLed, HIGH);
+    myservo.write(0);
+    delay(450);
+
+    digitalWrite(redLed, LOW);
+    myservo.write(90);
+  } else {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH); 
   }
-  // Tell the Arduino to wait 0.10 seconds before pinging the
-  // Ultrasonic Sensor again.
-  delay(100);
-}
-//Converts the Microseconds Reading to Centimeters
+
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+return microseconds / 29 / 2;
 }
-//This sub-routein is what dictates the movement of the servo
-void sweep() {
-/*
-  pos = lastpos;
- // pos = myservo.read();
-
-  if (lastpos < pos && pos <= 170) {
-    pos+=10;
-    if(pos=170){
-      p=1;
-    }
-  }
-
-  if (lastpos > pos && pos >= 10) {
-    pos-=10;
-    if(pos=10){
-      lastpos=9;
-    }
-  }
-
-  myservo.write(pos);
-*/
-  // Tells the Arduino to start this loop at 0 and incriment it by 1
-  // each time the loop completes. This is how the Arduino knows to
-  // stop the loop when a specific number of the
-  // Sweep routein has been ran.
-  // goes from 0 degrees to 180 degrees
-  for (pos = 10; pos < 170; pos += 1) {                // in steps of 1 degree
-    // tell servo to go to position in variable 'pos'
-    myservo.write(pos);
-    // waits 15ms for the servo to reach the position delay(10); }
-    // goes from 180 degrees to 0 degrees
-    for (pos = 170; pos >= 11; pos -= 1) {
-      // tell servo to go to position in variable 'pos'
-      myservo.write(pos);
-      // waits 15ms for the servo to reach the position
-      delay(10);
-    }
-  }  
 }
