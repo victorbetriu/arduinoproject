d4ad784819b2a6dc820f37ae2a8747c913ad9c29
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-13 14:33:40 +0100

    Update MOTOR_PROX_SERVO3.ino
    DIFFERENCES:
    1. We added the leds
    2. Servo + proximity works 
    
    TO CORRECT:
    1. To inicialize the position by a button. We try it with a for.
    2. Leds 
    3. Motor doesn't move correctly

--- MOTOR_PROX_SERVO3/MOTOR_PROX_SERVO3.ino
+++ MOTOR_PROX_SERVO3/MOTOR_PROX_SERVO3.ino
@@ -10,12 +10,11 @@ int bestValue; //millor distancia sensor
 int best;
 const int EchoPin = 5;
 const int TriggerPin = 6;
-//const int LedPin = 13;
 
+int  bestPosition;
 
 /////MOTOR
 #define VELOCIDAD 3500
-
 
 int steps = 2;
 int direccion = 3;
@@ -23,14 +22,23 @@ int reset = 12;
 int pasos = 200;
 int moviments = 3500;
 int Po = 0;
-int i=1;
-
-
 int dir = 1;
 int convert;
-
+int deplacament;
+int diferencia;
+int i = 0;
+int BState = 1;
 int button = 8;
-int buttonState = 1;
+
+
+////LEDS
+int  brightnessValue = 0;
+int fadeAmount = 20;
+int ledBe = 10;
+int ledMal = 11;
+
+
+
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -38,8 +46,8 @@ void setup() {
   ///SERVO+SENSOR
   Serial.begin(9600);
   myservo.attach(9);
+  myservo.write(pos);
 
-  //pinMode(LedPin, OUTPUT);
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT);
 
@@ -47,91 +55,97 @@ void setup() {
   pinMode(steps, OUTPUT);
   pinMode(direccion, OUTPUT);
   pinMode(reset, OUTPUT);
+
   pinMode(button, INPUT_PULLUP);
 
   //Posicio inici
-
   digitalWrite(reset, LOW);    //Mientras reset este en LOW el motor permanecerá apagado y no sufrirá. El chip apagará todos los puertos y no leerá comandos.
-  delay(100);
+  delayMicroseconds(100);
   digitalWrite(reset, HIGH);   //Cuando reset se encuentre en HIGH el motor arrancará y leerá los comandos enviados.
   digitalWrite(direccion, dir);
-  while (buttonState == 1) {
-    i++;   //Equivale al numero de vueltas (200 es 360º grados) o micropasos
+
+  while (BState == 1) { //mentre no s'apreti el botó gira 1 pas i llegeix el botó.
+    for(i=0;i<200;i++){
     digitalWrite(steps, HIGH);  // This LOW to HIGH change is what creates the
+    delayMicroseconds(100);
     digitalWrite(steps, LOW); // al A4988 de avanzar una vez por cada pulso de energia.
     delayMicroseconds(VELOCIDAD * 2); // Regula la velocidad, cuanto mas bajo mas velocidad.
-
-    buttonState = digitalRead(button);
+    BState = digitalRead(button);
+    }
   }
+
+  /////LEDS
+  pinMode(ledBe, OUTPUT); // Pin PWM para regular la base del transistor
+  pinMode(ledMal, OUTPUT);
+
+  Serial.println("Iniciando la fuente");
 }
 
 void loop() {
   Sensor();
+  //Motor();
+  leds();
 }
 
 void Sensor() {
+  if (millis() % 5 == 0) { // Cada 5ms:
+   
+    //LLEGEIX
+    cm = ping(TriggerPin, EchoPin); //mira distància: renova el valor
+    //Serial.println(cm);
 
-  if (millis() % 5 == 0) {
-    // llegir
-    // tell servo to go to position in variable 'pos'
-    cm = bestValue; //inicialitza el valor
-    Serial.print(bestValue);
+    if (pos >= 170 || pos <= 10) {//si està al final del recorregut
+      inc = -inc; //inverteix la direcció
+      bestPosition = best; //guarda la posició on hi ha una persona més propera de la 1/2 volta
+      bestValue = 100000;
+    }
 
-    cm = ping(TriggerPin, EchoPin);
-    Serial.print("Distancia: ");
-    Serial.println(cm);
-    delay(1000);
+    //ES MOU
+    pos += inc; //incrementa la posició
+    //Serial.println(pos);
+    myservo.write(pos); // mou el servo fins la posició
 
-    // new pos... escriure...
-    pos += inc;
-    Serial.println(pos);
-    if (pos > 170 || pos < 10) { //the servo arribes at the end of one cycle
-      inc = -inc;
+    //COMPARA
+    if (bestValue > cm) { //compara el valor antic amb l'actual. Si la distància actual és més propera...
+      bestValue = cm; // renova la millor distància
+      best = pos; // renova la posició amb la millor distància
       Motor();
     }
-
-    myservo.write(pos);
-
-    if (bestValue > cm) {
-      bestValue = cm;
-      best = pos;
-    }
-
-    // Serial.println(bestValue);
+    Serial.print("cm ");
+    Serial.println(cm);
+    delay(1);
   }
-  //  for (pos = 10; pos <= 170; pos += 5) { // goes from 0 degrees to 180 degrees
-  // in steps of 1 degree
-
-
-  //  Serial.println(best);
 }
+
 
 void Motor() {
-  moviments = map(best, 10, 170, 0, 200);
-  int diferencia = moviments - Po; //choose direction
-  if (diferencia > 0) {
-    dir = 1;
-  } if (diferencia <= 0) {
-    dir = 0;
+
+  if (millis() % 80 == 0) { //cada final de 1/2 volta del sensor+servo...
+    moviments = map( best, 10, 170, 0, 89); //Converteix els graus del servo a passos per l'stepper. Del rang de 160º del servo als equivalents 89 passos de 200.
+    diferencia = moviments - Po; //número de passos + o -
+    if (diferencia > 0) { //assigna direcció
+      dir = 1;
+    } if (diferencia <= 0) {
+      dir = 0;
+    }
+
+    Po = moviments; //renovem posició inici
   }
-  digitalWrite(reset, LOW);    //Mientras reset este en LOW el motor permanecerá apagado y no sufrirá. El chip apagará todos los puertos y no leerá comandos.
-  delay(100);
-  digitalWrite(reset, HIGH);   //Cuando reset se encuentre en HIGH el motor arrancará y leerá los comandos enviados.
+  //MOU STEPPER
   digitalWrite(direccion, dir);
-
-  int desplacament = abs(diferencia); //moves the specific distance
-
-  for (int i = 0; i < desplacament; i++)     //Equivale al numero de vueltas (200 es 360º grados) o micropasos
-  {
-    digitalWrite(steps, HIGH);  // This LOW to HIGH change is what creates the
-    digitalWrite(steps, LOW); // al A4988 de avanzar una vez por cada pulso de energia.
-    delayMicroseconds(VELOCIDAD);     // Regula la velocidad, cuanto mas bajo mas velocidad.
-
+  while (abs(diferencia) >= i) {
+    digitalWrite(steps, HIGH);
+    digitalWrite(steps, LOW);
+    i++;
   }
-  Po = moviments;
-  bestValue = 10000;
-}
 
+  i = 0; //refresquem valor
+  ///// RESET DEL MOTOR
+  //  digitalWrite(reset, LOW);    //Mientras reset este en LOW el motor permanecerá apagado y no sufrirá. El chip apagará todos los puertos y no leerá comandos.
+  //  delayMicroseconds(100);
+  //  digitalWrite(reset, HIGH);   //Cuando reset se encuentre en HIGH el motor arrancará y leerá los comandos enviados.
+  //  digitalWrite(direccion, dir);
+}
 
 
 int ping(int TriggerPin, int EchoPin) {
@@ -150,4 +164,27 @@ int ping(int TriggerPin, int EchoPin) {
 }
 
 
+void leds() {
+  constrain(brightnessValue, 0, 255); //limitem la intensitat de la llum
+
+  if (micros() % 5000 == 0) { //cada 1/2ms...
+    brightnessValue = brightnessValue + fadeAmount; //agumentem o disminuim 20
+
+    if (cm < 50) { //il·luminem la imatge bona
+      Serial.println("FUNCIONA");
+      analogWrite(ledBe,  brightnessValue);
+      analogWrite(ledMal, 0);
+      //Serial.println(fadeAmount);
+    }  else {
+      //Serial.println("HOLA");
+      analogWrite(ledMal,  brightnessValue);
+      analogWrite(ledBe,  0);
+      //Serial.println(fadeAmount);
+    }
+  }
+
+  if (brightnessValue >= 255 || brightnessValue <= 0) {
+    fadeAmount = -fadeAmount;
+  }
+}
 
