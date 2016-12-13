51cdec60d3d6d7002d47aa0b507ee40d261f32fc
helenaestapegrau <helena.estape@epiagranollers.cat>
2016-12-13 09:31:20 +0100

    LEDS OFICIAL

--- tira_leds_fading.ino
+++ tira_leds_fading.ino
@@ -1,5 +1,6 @@
-int brightnessValue;
-int ledPin = 3;
+int  brightnessValue;
+int fadeAmount = 20;
+int ledPin =3;
 
 void setup()
 { Serial.begin(9600);
@@ -10,20 +11,21 @@ void setup()
 
 void loop() {
   constrain(brightnessValue, 0, 255); //limit the value (0 - 255)
-  //INCREASE THE BRIGHTNESS
-  if (millis() % 1 == 0) { //Everytime...
-    if (millis() % 560 < 256) { //If the residu is lower than 256 (max. brightess value of a led)
-      brightnessValue ++; //increase 1 the brightness
-      delay(1); //do it once per milli
-    }
-    //DECREASE THE BRIGHTNESS
-    if (millis() % 560 > 255) { //If the residu is higher than 255...
-      brightnessValue --; //decrease 1 the brightness
-      delay(1); 
+  //INCREASE or DECREASES THE BRIGHTNESS EVERY MILLI
+  if (millis() % 1 == 0) {
+   brightnessValue = brightnessValue + fadeAmount;
+    delay(1);
+  } 
+   int clau = 510/fadeAmount; //255*2/fadeAmount: the millis when the brightness is max. or min.
+  //CHANGE THE SIGN: + OR -
+  if (millis() % clau >= 0) { //When brightess value is max. or min. it changes the sign
+    if (brightnessValue >= 255 || brightnessValue <= 0) {
+      fadeAmount = -fadeAmount;
     }
     delay(1);
   }
-  analogWrite(pinLeds, brightnessValue); //Make led show the corresponent brightness
+  analogWrite(10,  brightnessValue);
+  Serial.println(fadeAmount);
 }
 
 
